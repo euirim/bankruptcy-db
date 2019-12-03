5c22a6a1e59fbe828b917610df86325288755d4c
Euirim Choi <euirim@me.com>
2019-12-03 04:19:07 -0800

    Reduce number of entries displayed in admin

--- bankruptcy/cases/admin.py
+++ bankruptcy/cases/admin.py
@@ -4,16 +4,19 @@ from .models import Case, DocketEntry, Document
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
     list_display = ["name", "recap_id", "date_filed", "jurisdiction", "chapter"]
-    search_fields = ["name", "chapter", "jurisdiction", "recap_id"]
+    search_fields = ["name", "chapter", "jurisdiction", "recap_id", "pacer_id"]
+    list_per_page = 25
 
 @admin.register(DocketEntry)
 class DocketEntryAdmin(admin.ModelAdmin):
     list_display = ["recap_id", "case", "date_filed"]
     search_fields = ["recap_id", "description"]
     raw_id_fields = ["case"]
+    list_per_page = 25
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     list_display = ["recap_id", "pacer_id", "doc_type", "is_available"]
     search_fields = ["description", "text", "pacer_id"]
     raw_id_fields = ["docket_entry"]
+    list_per_page = 25
